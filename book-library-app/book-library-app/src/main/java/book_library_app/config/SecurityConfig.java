7626ae4292f80cf5c0ac74847a5bc6139c707591
m0niq <monikcho0o@abv.bg>
2025-12-03 11:39:35 +0000

    fix:Security

--- book-library-app/book-library-app/src/main/java/book_library_app/config/SecurityConfig.java
+++ book-library-app/book-library-app/src/main/java/book_library_app/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+               // .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/", "/index", "/home",
